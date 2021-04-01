67b383591baf2d35da40fb364aae9c6b8ffda70d
Mikeias Silva <mikeias26@gmail.com>
2021-04-01 20:11:10 -0300

    finished api-api-front

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const cors = require('cors')
 const express = require('express')
 const app = express()
-
+const axios = require('axios')
 
 app.use(cors())
 
-app.get('/', function (req, res) {
-    return res.json([
-        {
-            name: "mikeias"
-        },
-        {
-            name: "joainha"
-        }
-    ])
-} )
 
-app.listen('5001', function(){
+
+
+app.get('/', async function (req, res) {
+    try {
+        const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+        // console.log(response)
+
+        // const data = response.data
+
+        return res.json(data)
+    } catch (error) {
+        console.error(error)
+    }
+
+
+
+
+})
+
+app.listen('5001', function () {
     console.log("porta 5001 on")
 })
